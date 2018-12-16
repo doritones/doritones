9cce64e2b885240ca366737e5b885e0e92dd3a1d
Denis <denisaraki@gmail.com>
2018-12-16 21:45:38 +0100

    XPTO

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 // app.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
-// initialize our express app
 
 const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://someuser:abcd1234@ds135974.mlab.com:35974/scaramouche';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
 let port = 1234;
--- routes/product.route.js
+++ routes/product.route.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const product_controller = require('../controllers/product.controller');
+router.post('/create', product_controller.product_create);
 
+router.delete('/:id/delete', product_controller.product_delete);
+
+router.put('/:id/update', product_controller.product_update);
+
+router.get('/:id', product_controller.product_details);
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', product_controller.test);
