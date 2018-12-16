3895357dfbc45b13337efac14cdc486691b1f12f
Denis <denisaraki@gmail.com>
2018-12-16 19:14:58 +0100

    Update app.js
    Inserted missing lines

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 // initialize our express app
+
+const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 
 app.use('/products', product);
